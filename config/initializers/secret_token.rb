fec6c98053aec0c181abcd5b52b5283110763591
Christoph, René Pardon <rp@padcloud.com>
2013-07-18 11:07:19 +0200

    Initial commit of 3.2.14rc2 Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ActivescaffoldTestApi::Application.config.secret_token = 'f6b3459ea1e5e3b6afe7ec131b1a87afdfabf0306dce1b820aca765cef2efb78e1f205b326d7c743828d598e8e2ac64a939ce7fabb80c992f275b4bd74afd47d'
